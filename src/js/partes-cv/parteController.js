9a993c876ba56dbdd40b981fc31d97aa0a29e6ca
Juan José Molano <molano.juan.2629@eam.edu.co>
2026-04-07 10:45:32 -0500

    Indentación

--- src/js/partes-cv/parteController.js
+++ src/js/partes-cv/parteController.js
@@ -1,29 +1,29 @@
 export class ParteController {
 
     store(data, cvPart) {
-    const stored = localStorage.getItem("parts");
-    const parts = stored ? JSON.parse(stored) : [];
+        const stored = localStorage.getItem("parts");
+        const parts = stored ? JSON.parse(stored) : [];
 
-    const user = localStorage.getItem("currentUser");
+        const user = localStorage.getItem("currentUser");
 
-    const fullData = {
-        ...data,
-        user,
-        cvPart
-    };
+        const fullData = {
+            ...data,
+            user,
+            cvPart
+        };
 
-    const index = parts.findIndex(p =>
-        p.user === user &&
-        p.cvPart === cvPart
-    );
+        const index = parts.findIndex(p =>
+            p.user === user &&
+            p.cvPart === cvPart
+        );
 
-    if (index !== -1) {
-        parts[index] = fullData;
-    } else {
-        parts.push(fullData);
+        if (index !== -1) {
+            parts[index] = fullData;
+        } else {
+            parts.push(fullData);
+        }
+
+        localStorage.setItem("parts", JSON.stringify(parts));
+        return true;
     }
-
-    localStorage.setItem("parts", JSON.stringify(parts));
-    return true;
-}
 }
