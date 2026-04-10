b080cc6d5ff5fa89cccfa091e9ea139fe5a3a5b6
Juan José Molano <molano.juan.2629@eam.edu.co>
2026-04-09 19:45:21 -0500

    Añade parte 2 del formulario

--- src/html/partes-hoja-de-vida/2-formacion-academica.js
+++ src/html/partes-hoja-de-vida/2-formacion-academica.js
@@ -16,24 +16,42 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
-const today = new Date().toISOString().split("T")[0];
-document.getElementById("fechaGrado").setAttribute("max", today);
-
 function run() {
 
     const educacionBasica = document.getElementById("educacionBasica")?.value || "";
     const titulo = document.getElementById("titulo").value.trim();
     const fechaGrado = document.getElementById("fechaGrado").value.trim();
-    
-    //const tipoDocumento = document.querySelector('input[name="tipoDocumento"]:checked')?.value || "";
+    const tablaFormacion = controller.getTablaFormacion();
+    const tablaIdiomas = controller.getTablaIdiomas();
 
     let invalid = false;
 
-    if (validator.titulo(eductituloacionBasica)) {
+    if (validator.titulo(titulo)) {
         document.getElementById("error-titulo").textContent = "El titulo es inválido";
         invalid = true;
     } else {
         document.getElementById("error-titulo").textContent = "";
+    }
+
+    if (validator.fechaGrado(fechaGrado)) {
+        document.getElementById("error-fechaGrado").textContent = "La fecha es inválida";
+        invalid = true;
+    } else {
+        document.getElementById("error-fechaGrado").textContent = "";
+    }
+
+    if (validator.tablaFormacion(tablaFormacion)) {
+        document.getElementById("error-tablaFormacion").textContent = "Hay al menos un campo inválido";
+        invalid = true;
+    } else {
+        document.getElementById("error-tablaFormacion").textContent = "";
+    }
+
+    if (validator.tablaIdiomas(tablaIdiomas)) {
+        document.getElementById("error-tablaIdiomas").textContent = "Hay al menos un campo inválido";
+        invalid = true;
+    } else {
+        document.getElementById("error-tablaIdiomas").textContent = "";
     }
 
     if (invalid) return;
@@ -41,7 +59,9 @@ function run() {
     const data = {
         educacionBasica,
         titulo,
-        fechaGrado
+        fechaGrado,
+        tablaFormacion,
+        tablaIdiomas
     };
 
     const currentPart = 2;
@@ -63,3 +83,7 @@ document.addEventListener("DOMContentLoaded", () => {
         educacionBasicaSelect.appendChild(option);
     });
 });
+
+document.getElementById("addRowTablaFormacion").addEventListener("click", controller.addRowTablaFormacion);
+
+document.getElementById("addRowTablaIdiomas").addEventListener("click", controller.addRowTablaIdiomas);
